ea19592082e51bf444b68a4b54236ee478309b88
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-11-16 13:56:20 +0300

    implemented review comments

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,14 +4,16 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-  let newArray = [...arr];
-  const locale = ['ru', 'en'];
-  const options = {caseFirst: 'upper'};
 
-  const ascendingSorting = (arr) => arr.sort((a, b) => a.localeCompare(b, locale, options));
-  const descendingSorting = (arr) => arr.sort((a, b) => b.localeCompare(a, locale, options));
-  
+const LOCALE = ['ru', 'en'];
+const OPTIONS = {caseFirst: 'upper'};
+
+const ascendingSorting = (arr) => arr.sort((a, b) => a.localeCompare(b, LOCALE, OPTIONS));
+const descendingSorting = (arr) => arr.sort((a, b) => b.localeCompare(a, LOCALE, OPTIONS));
+
+export function sortStrings(arr, param = 'asc') {
+  const newArray = [...arr];
+
   return param === 'asc' ? ascendingSorting(newArray) : descendingSorting(newArray);
 
 }
