5cc4108d24eb8ddb735db85c71ff5ff574af1409
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-11-20 23:03:39 +0300

    yet another fix to MR

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,18 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-  const pathArray = path.split(".");
-    
-  return getter = (obj) => {
-    let result = obj;
-    
-    for (const key of pathArray) {
-      if (typeof result === 'object' && Object.hasOwn(result, key)) {
-        result = result[key];
-      } else {
-        return;
-      }
-    }
-    return result;
+  const a = path.split(".");
+  return function (o) {
+    return a.reduce((o, k) => (o && k ? o[k] : undefined), o);
   };
 }
