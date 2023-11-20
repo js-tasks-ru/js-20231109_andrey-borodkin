e8b849e4fdf6316e2b7af3868d65d5e3a71a1437
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-11-20 22:47:52 +0300

    another try to fix test issues

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,18 +3,25 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
-  const pathArray = path.split(".");
+// export function createGetter(path) {
+//   const pathArray = path.split(".");
     
-  return getter = (obj) => {
+//   return getter = (obj) => {
 
-    for (const key of pathArray) {
-      if (obj && obj.hasOwnProperty(key)) {
-        obj = obj[key];
-      } else {
-        return;
-      }
-    }
-    return obj;
+//     for (const key of pathArray) {
+//       if (obj && obj.hasOwnProperty(key)) {
+//         obj = obj[key];
+//       } else {
+//         return;
+//       }
+//     }
+//     return obj;
+//   };
+// }
+
+export function createGetter(path) {
+  const a = path.split(".");
+  return function (o) {
+    return a.reduce((o, k) => (o && k ? o[k] : undefined), o);
   };
 }
