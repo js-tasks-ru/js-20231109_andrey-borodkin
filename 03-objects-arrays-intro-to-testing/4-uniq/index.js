05dabd9530b82399dcd2436d4a3572477c760165
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-11-20 13:40:31 +0300

    module 3 hw complete

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,25 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-
+export function uniq(arr = []) {
+  let tempArray = [];
+  //   if (!(arr instanceof Array)) {
+  //     return tempArray; 
+  //   }
+  for (const elem of arr) {
+    if (!tempArray.includes(elem)) {
+      tempArray.push(elem);
+    }
+  }
+  return tempArray;
 }
+
+
+export const uniq2 = (arr) => Array.isArray(arr) ? arr.reduce((acc, item) => {
+  if (!acc.includes(item)) {
+    acc.push(item);
+  }
+  return acc;
+}, []) : [] ;
+
+export const uniq3 = (arr = []) => Array.from(new Set(arr));
