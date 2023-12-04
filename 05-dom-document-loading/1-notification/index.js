e4734692c09da33d0fe1ecf477b33e3f1f2f094e
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-12-04 16:38:09 +0300

    solution to hw4-1NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,71 @@
 export default class NotificationMessage {
 
+    static currentNotification = null
+    
+    constructor(
+      message = '',
+      type = {
+        duration: 0,
+        type: ''
+      }
+    ) {
+
+      this.message = message;
+      this.duration = type.duration;
+      this.type = type.type;    
+
+      this.element = this.createElement(this.createTemplate());
+    }
+
+
+    createTemplate() {
+      return (
+        `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+
+      `);
+    }
+
+    createElement(template) {
+      const element = document.createElement('div');
+      element.innerHTML = template;
+
+      return element;
+    }
+
+    show(target = document.body) {
+      if (NotificationMessage.currentNotification) {
+        NotificationMessage.currentNotification.remove();
+      }
+
+      target.append(this.element);
+      NotificationMessage.currentNotification = this;
+
+      this.timer = setTimeout(() => {
+        this.remove();
+      }, this.duration); 
+    }
+
+
+
+    destroy() {
+        clearTimeout(this.timer)
+      this.remove();
+
+      if (NotificationMessage.currentNotification === this) {
+        NotificationMessage.currentNotification = null
+      }
+    }
+
+    remove() {
+      this.element.remove();
+    }
 }
