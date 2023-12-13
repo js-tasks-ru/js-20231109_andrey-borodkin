420b135f71ac1d8b6396c0b0b86a81b5c33007dd
Andrey Borodkin <andrey.borodkin@axenix.pro>
2023-12-13 11:39:39 +0300

    fixes of hw5

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,110 +1,105 @@
-// import { template } from "@babel/core";
-
 export default class SortableTable {
-  subElements = {};
-  fieldValue
-  orderValue
-
   constructor(headerConfig = [], data = []) {
     this.headerConfig = headerConfig;
     this.data = data;
     this.element = this.createElement(this.createTemplate());
 
+    this.subElements = {
+      body: this.element.querySelector('[data-element="body"]')
+    };
   }
 
   createElement(template) {
     const element = document.createElement("div");
-    
     element.innerHTML = template;
-
     return element.firstElementChild;
   }
 
-
-  createSortedIconTemplate(id) {
-    if (this.fieldValue === 'id') {
-      return (`
-               <span data-element="arrow" class="sortable-table__sort-arrow">
-                 <span class="sort-arrow"></span>
-               </span>
-      `);
-    }
-    return '';
+  createTemplate() {
+    return `
+      <div class="sortable-table">
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+          ${this.createHeaderTemplate()}
+        </div>
+        <div data-element="body" class="sortable-table__body">
+          ${this.createBodyTemplate()}
+        </div>
+      </div>`;
   }
 
   createHeaderTemplate() {
     return this.headerConfig.map((column) => {
-      return (`
-              <div class="sortable-table__cell" data-id="${column.id}" data-sortable="${column.sortable}"  data-order="${this.fieldValue === column.id ? this.orderValue : ''}">
-                <span>${column.title}</span>
-                ${this.createSortedIconTemplate(column.id)}
-              </div>
-      `);
+      return `
+        <div class="sortable-table__cell" 
+          data-id="${column.id}" 
+          data-sortable="${column.sortable}" 
+          data-order="${this.fieldValue === column.id ? this.orderValue : ''}"
+        >
+          <span>${column.title}</span>
+        </div>
+      `;
     }).join(' ');
+  }
 
+  createBodyRowCellsTemplate(item) {
+    return this.headerConfig.map((config) => {
+      const fieldName = config.id;
+      const fieldValue = item[fieldName];
+
+      if (config.template) {
+        return config.template(fieldValue);
+      }
+
+      return `
+        <div class="sortable-table__cell">${fieldValue}</div>
+      `;
+    }).join();
+  }
+
+  createBodyRowTemplate(item) {
+    return `
+      <a href="/products/${item.id}" class="sortable-table__row">
+        ${this.createBodyRowCellsTemplate(item)}
+      </a>`;
   }
 
   createBodyTemplate() {
-    
-    return this.data.map((item, index) => {
-      
-      return (`
-                <a href="/products/${item.id}" class="sortable-table__row">
-                  ${this.headerConfig[0].template(this.data[index])}
-                  <div class="sortable-table__cell">${item.title}</div>
-                  <div class="sortable-table__cell">${item.quantity}</div>
-                  <div class="sortable-table__cell">${item.price}</div>
-                  <div class="sortable-table__cell">${item.sales}</div>
-                </a>
-      `);
-    }).join(' ');
+    return this.data.map((item) => 
+      this.createBodyRowTemplate(item)
+    ).join('');
   }
-
-  createTemplate() {
-
-    return `<div class="sortable-table">
-              <div data-element="header" class="sortable-table__header sortable-table__row">
-                ${this.createHeaderTemplate()}
-              </div>
-              <div data-element="body" class="sortable-table__body">
-                ${this.createBodyTemplate()}
-              </div>
-            </div>`;
-  }
-
 
   sort(fieldValue, orderValue) {
-    
-    this.fieldValue = fieldValue;
-    this.orderValue = orderValue;
-
     const LOCALE = ['ru', 'en'];
-    const OPTIONS = {caseFirst: 'upper'};
+    const OPTIONS = { caseFirst: 'upper' };
 
     const sortAscString = (a, b) => a.title.localeCompare(b.title, LOCALE, OPTIONS);
     const sortDescString = (a, b) => b.title.localeCompare(a.title, LOCALE, OPTIONS);
-    
     const sortAscNumber = (a, b) => b[fieldValue] < a[fieldValue] ? 1 : -1;
     const sortDescNumber = (a, b) => b[fieldValue] < a[fieldValue] ? -1 : 1;
 
+    const { sortType } = this.headerConfig.find(config => config.id === fieldValue);
 
-
-    if (fieldValue === 'title') {
+    if (sortType === 'string') {
       this.data.sort(orderValue === 'asc' ? sortAscString : sortDescString);
-    } else {
+    }
+
+    if (sortType === 'number') {
       this.data.sort(orderValue === 'asc' ? sortAscNumber : sortDescNumber);
     }
-    
 
     this.update();
   }
 
   update() {
-    this.element.querySelector('[data-element="body"]').innerHTML = this.createBodyTemplate();
+    this.subElements.body.innerHTML = this.createBodyTemplate();
   }
 
-  destroy() {
+  remove() {
     this.element.remove();
   }
 
+  destroy() {
+    this.remove();
+  }
 }
